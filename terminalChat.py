aef46e26f5558686769b335266f49ee63e74ab92
vincentmuya <vincentmuya13@gmail.com>
2019-10-15 22:43:19 +0300

    created terminalChat.py and created class terminal chat

---
+++ terminalChat.py
@@ -0,0 +1,25 @@
+from pusher import Pusher
+import pysher
+import os
+import json
+import getpass
+from temcolor import colored
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path='.env')
+
+class terminalChat():
+    pusher = None
+    channel = None
+    chatroom= None
+    clientPusher = None
+    user = None
+    users = {
+        "samuel": "samuel'spassword",
+        "daniel": "daniel'spassword",
+        "tobi": "tobi'spassword",
+        "sarah": "sarah'spassword"
+
+    }
+    chatrooms =["sports", "general", "education", "health", "technology"]
